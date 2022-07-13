4c97d366f3f48fc9a962de74e2e796ea8cdd6adc
重生之我在 沈阳大街当老六 <2022337524@qq.com>
2022-07-13 16:45:04 +0800

    创建导航栏和游戏界面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView'
+import RankListIndexView from '../views/ranklist/RankListIndexView'
+import RecordIndexView from '../views/record/RecordIndexView'
+import NotFoundView from '../views/error/NotFoundView'
+import UserBotIndexView from '../views/user/bot/UserBotIndexView'
+//import { redirect } from 'statuses'
 
+//匹配是从上往下匹配的
 const routes = [
-
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/"   //重定向 即用户输入xxx.com/  将输入变成 xxx.com/pk/ 重新发送请求
+  },
+  {
+    path:"/pk/",
+    name:"pk_index",
+    component:  PkIndexView,
+  },
+  {
+    path:"/record/",
+    name: "record_index",
+    component:  RecordIndexView,
+  },
+  {
+    path:"/ranklist/",
+    name: "ranklist_index",
+    component:  RankListIndexView,
+  },
+  {
+    path:"/user/bot/",
+    name: "user_bot_index",
+    component:  UserBotIndexView,
+  },
+  {
+    path:"/404/",
+    name: "404",
+    component:  NotFoundView,
+  },
+  {
+    path:"/:catchall(.*)",//匹配任意字符
+    redirect: "/404/"  //重定向到404页面
+  }
 ]
 
 const router = createRouter({
---
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -0,0 +1,19 @@
+<template>
+    <ContentField>
+        我的蛇蛇
+    </ContentField>
+</template>
+
+<script>
+import ContentField from '../../../components/ContentField.vue'
+
+export default {
+    components: {
+        ContentField,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
